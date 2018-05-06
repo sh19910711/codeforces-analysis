6cf2c93c495d7df2131961d61d66de1d319e24f7
Hiroyuki Sano <sh19910711@gmail.com>
2018-05-06 09:10:27 +0000

    add problems

---
+++ functions/problems.py
@@ -0,0 +1,46 @@
+import boto3
+from botocore.vendored import requests
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+firehose = boto3.client('firehose')
+
+def parse(text):
+    ret = json.loads(text)
+
+    if ret['status'] != 'OK':
+        raise 'ERROR: api status = {}'.format(ret['status'])
+
+    chunk = []
+    for s in ret['result']['problems']:
+        # "contestId", "index", "name", "type", "tags"
+        for t in s['tags']:
+            chunk.append({
+                'contest_id': s['contestId'],
+                'problem_index': s['index'],
+                'problem_tag': t
+            })
+
+            if len(chunk) >= 500:
+                yield chunk
+                chunk = []
+
+    if len(chunk) > 0:
+        yield chunk
+
+    return ()
+
+def handler(event, context):
+    url = 'http://codeforces.com/api/problemset.problems'
+    logger.info('GET: {}'.format(url))
+    res = requests.get(url)
+
+    for chunk in parse(res.text):
+        firehose.put_record_batch(
+            DeliveryStreamName='codeforces-analysis-problems',
+            Records=[{ 'Data': json.dumps(status) + "\n" } for status in chunk]
+        )
+
+    return 'OK'
